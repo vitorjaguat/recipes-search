20b1c2a706f728d78c326a3701ad05fd2ca2c2d9
Vitor Jaguat <vitorbutkus@gmail.com>
2022-08-14 18:49:55 -0300

    add search engine, page results, view recipe etc

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+//This module contains and exports variables that are used several times all over the app. If the variables change in the future, for example the url for the API, then we only have to update it here.
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,66 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime'
+
+if (module.hot) {
+  module.hot.accept();
+}
+//Hot Module Replacement (HMR) improves the development experience by automatically updating modules in the browser at runtime without needing a whole page refresh. This means that application state can be retained as you change small things. Parcel's HMR implementation supports both JavaScript and CSS assets.
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    //window.location is the whole url address!
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //1) Loading recipe:
+    await model.loadRecipe(id);
+
+
+    //2) Rendering recipe:
+    recipeView.render(model.state.recipe);
+
+  } catch (err) {
+    recipeView.renderError();
+  }
 };
+controlRecipes()
 
-// https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+// ['load', 'hashchange'].forEach(ev => window.addEventListener(ev, controlRecipes));
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    //1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2) Load search results
+    await model.loadSearchResults(query);
+
+    //3) Render results
+    // console.log(model.state.search.results);
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(1));
+
+    //4) Render initial pagination buttons:
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+}
+init();
